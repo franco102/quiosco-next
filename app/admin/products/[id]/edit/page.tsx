3d93b5c23d78dc0fe2226e6b9ea272fc0206ca34
fsilva <fsilva@urbano.com.pe>
2025-01-07 16:56:39 -0500

    first commit

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -18,11 +18,17 @@ async function getProductById(id:number) {
     return product
 }
 
-export default async function EditProductPage({params}:Awaited<{
-  params: Promise<{ id: string }>;
-}>)  {
-    const productParam=await params;
-    const product=await getProductById(+productParam.id) 
+export default async function EditProductPage({
+  searchParams,
+}: {
+  searchParams: { id: string };
+}) {
+  // Convertir el ID de los parámetros de búsqueda a número
+  const productId = parseInt(searchParams.id, 10);
+  if (isNaN(productId)) {
+    notFound(); // Manejo de error si no es un número válido
+  }
+    const product=await getProductById(productId) 
   return (
     <>
           <Heading>Editar Producto: {product.name}</Heading>
